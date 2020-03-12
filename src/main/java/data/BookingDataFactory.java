70b4434745b4c86d395a8b8276e740a38c69083a
eliasnogueira <elias.nogueira@gmail.com>
2020-03-12 08:56:36 +0100

    license added

--- src/main/java/data/BookingDataFactory.java
+++ src/main/java/data/BookingDataFactory.java
@@ -1,14 +1,38 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Elias Nogueira
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package data;
 
 import com.aventstack.extentreports.service.ExtentTestManager;
 import com.github.javafaker.Faker;
 import config.Configuration;
+import config.ConfigurationManager;
 import enums.RoomType;
 import java.util.Locale;
 import java.util.Random;
 import lombok.extern.log4j.Log4j2;
 import model.Booking;
-import org.aeonbits.owner.ConfigCache;
 
 @Log4j2
 public class BookingDataFactory {
@@ -16,7 +40,7 @@ public class BookingDataFactory {
     private final Faker faker;
 
     public BookingDataFactory() {
-        Configuration configuration = ConfigCache.getOrCreate(Configuration.class);
+        Configuration configuration = ConfigurationManager.getConfiguration();
         faker = new Faker(new Locale(configuration.faker()));
     }
 
@@ -37,11 +61,11 @@ public class BookingDataFactory {
     }
 
     private String returnRandomCountry() {
-        return returnRandomItemOnArray(new String[] {"Belgium", "Brazil", "Netherlands"});
+        return returnRandomItemOnArray(new String[]{"Belgium", "Brazil", "Netherlands"});
     }
 
     private String returnDailyBudget() {
-        return returnRandomItemOnArray(new String[] {"$100", "$100 - $499", "$499 - $999", "$999+"});
+        return returnRandomItemOnArray(new String[]{"$100", "$100 - $499", "$499 - $999", "$999+"});
     }
 
     private String returnRandomItemOnArray(String[] array) {
