4db9e782f5f5117b27ea92634c989384f41b0f8c
Elias Nogueira <elias.nogueira@gmail.com>
2021-03-13 16:56:59 +0100

    new browser factory class

---
+++ src/main/java/com/eliasnogueira/driver/BrowserFactory.java
@@ -0,0 +1,165 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Elias Nogueira
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.eliasnogueira.driver;
+
+import com.eliasnogueira.exceptions.HeadlessNotSupportedException;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.config.DriverManagerType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+import org.openqa.selenium.remote.AbstractDriverOptions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
+
+import static com.eliasnogueira.config.ConfigurationManager.configuration;
+import static java.lang.Boolean.TRUE;
+
+public enum BrowserFactory {
+
+    CHROME {
+        @Override
+        public WebDriver createDriver() {
+            WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
+
+            return new ChromeDriver(getOptions());
+        }
+
+        @Override
+        public ChromeOptions getOptions() {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments(START_MAXIMIZED);
+            chromeOptions.addArguments("--disable-infobars");
+            chromeOptions.addArguments("--disable-notifications");
+            chromeOptions.setHeadless(configuration().headless());
+
+            return chromeOptions;
+        }
+    }, FIREFOX {
+        @Override
+        public WebDriver createDriver() {
+            WebDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
+
+            return new FirefoxDriver(getOptions());
+        }
+
+        @Override
+        public FirefoxOptions getOptions() {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.addArguments(START_MAXIMIZED);
+            firefoxOptions.setHeadless(configuration().headless());
+
+            return firefoxOptions;
+        }
+    }, EDGE {
+        @Override
+        public WebDriver createDriver() {
+            WebDriverManager.getInstance(DriverManagerType.EDGE).setup();
+
+            return new EdgeDriver(getOptions());
+        }
+
+        @Override
+        public EdgeOptions getOptions() {
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.addArguments(START_MAXIMIZED);
+            edgeOptions.setHeadless(configuration().headless());
+
+            return edgeOptions;
+        }
+    }, SAFARI {
+        @Override
+        public WebDriver createDriver() {
+            WebDriverManager.getInstance(DriverManagerType.SAFARI).setup();
+
+            return new SafariDriver(getOptions());
+        }
+
+        @Override
+        public SafariOptions getOptions() {
+            SafariOptions safariOptions = new SafariOptions();
+            safariOptions.setAutomaticInspection(false);
+
+            if (TRUE.equals(configuration().headless()))
+                throw new HeadlessNotSupportedException(safariOptions.getBrowserName());
+
+            return safariOptions;
+        }
+    }, OPERA {
+        @Override
+        public WebDriver createDriver() {
+            WebDriverManager.getInstance(DriverManagerType.OPERA).setup();
+
+            return new OperaDriver(getOptions());
+        }
+
+        @Override
+        public OperaOptions getOptions() {
+            OperaOptions operaOptions = new OperaOptions();
+            operaOptions.addArguments(START_MAXIMIZED);
+            operaOptions.addArguments("--disable-infobars");
+            operaOptions.addArguments("--disable-notifications");
+
+            if (TRUE.equals(configuration().headless()))
+                throw new HeadlessNotSupportedException(operaOptions.getBrowserName());
+
+            return operaOptions;
+        }
+    }, IE {
+        @Override
+        public WebDriver createDriver() {
+            WebDriverManager.getInstance(DriverManagerType.IEXPLORER).setup();
+
+            return new InternetExplorerDriver(getOptions());
+        }
+
+        @Override
+        public InternetExplorerOptions getOptions() {
+            InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
+            internetExplorerOptions.ignoreZoomSettings();
+            internetExplorerOptions.takeFullPageScreenshot();
+            internetExplorerOptions.introduceFlakinessByIgnoringSecurityDomains();
+
+            if (TRUE.equals(configuration().headless()))
+                throw new HeadlessNotSupportedException(internetExplorerOptions.getBrowserName());
+
+            return internetExplorerOptions;
+        }
+    };
+
+    private static final String START_MAXIMIZED = "--start-maximized";
+
+    public abstract WebDriver createDriver();
+
+    public abstract AbstractDriverOptions<?> getOptions();
+}
