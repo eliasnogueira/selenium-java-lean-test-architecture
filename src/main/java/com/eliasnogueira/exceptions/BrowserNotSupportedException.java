088365aabb7fd83839d38fd18d2054509f148e09
Elias Nogueira <elias.nogueira@gmail.com>
2021-03-13 16:56:25 +0100

    remove custom exception

--- src/main/java/com/eliasnogueira/exceptions/BrowserNotSupportedException.java
+++
@@ -1,8 +0,0 @@
-package com.eliasnogueira.exceptions;
-
-public class BrowserNotSupportedException extends IllegalStateException {
-
-    public BrowserNotSupportedException(String browser) {
-        super(String.format("Browser not supported: %s", browser));
-    }
-}
