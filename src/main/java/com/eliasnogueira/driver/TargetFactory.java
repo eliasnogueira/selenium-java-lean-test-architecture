185af2e4319edfaf2c5df3e262599cd5198e6fc0
Elias Nogueira <elias.nogueira@gmail.com>
2022-09-20 22:00:43 +0200

    general refactor

--- src/main/java/com/eliasnogueira/driver/TargetFactory.java
+++ src/main/java/com/eliasnogueira/driver/TargetFactory.java
@@ -24,7 +24,6 @@
 
 package com.eliasnogueira.driver;
 
-import com.eliasnogueira.exceptions.TargetNotValidException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.MutableCapabilities;
@@ -41,19 +40,11 @@ public class TargetFactory {
 
     public WebDriver createInstance(String browser) {
         Target target = Target.valueOf(configuration().target().toUpperCase());
-        WebDriver webdriver;
 
-        switch (target) {
-            case LOCAL:
-                webdriver = BrowserFactory.valueOf(browser.toUpperCase()).createDriver();
-                break;
-            case REMOTE:
-                webdriver = createRemoteInstance(BrowserFactory.valueOf(browser.toUpperCase()).getOptions());
-                break;
-            default:
-                throw new TargetNotValidException(target.toString());
-        }
-        return webdriver;
+        return switch (target) {
+            case LOCAL -> BrowserFactory.valueOf(browser.toUpperCase()).createDriver();
+            case REMOTE -> createRemoteInstance(BrowserFactory.valueOf(browser.toUpperCase()).getOptions());
+        };
     }
 
     private RemoteWebDriver createRemoteInstance(MutableCapabilities capability) {
