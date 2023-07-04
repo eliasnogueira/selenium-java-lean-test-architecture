9e7e370bcb5fbb591de878aa7aa873794f2a9828
mdiwakar <mbdiwakar@gmail.com>
2023-07-04 15:38:50 -0700

    188: removed whitelines

--- src/main/java/com/eliasnogueira/driver/BrowserFactory.java
+++ src/main/java/com/eliasnogueira/driver/BrowserFactory.java
@@ -38,8 +38,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.safari.SafariOptions;
 import org.testcontainers.containers.BrowserWebDriverContainer;
-
-
 import static com.eliasnogueira.config.ConfigurationManager.configuration;
 import static com.eliasnogueira.data.changeless.BrowserData.CHROME_HEADLESS;
 import static com.eliasnogueira.data.changeless.BrowserData.DISABLE_INFOBARS;
@@ -65,8 +63,6 @@ public enum BrowserFactory {
                     .withCapabilities(new ChromeOptions());
                 driverContainer.start();
                 return new RemoteWebDriver(driverContainer.getSeleniumAddress(), new ChromeOptions());
-
-
         }
         @Override
         public ChromeOptions getOptions() {
@@ -75,16 +71,13 @@ public enum BrowserFactory {
             chromeOptions.addArguments(DISABLE_INFOBARS);
             chromeOptions.addArguments(DISABLE_NOTIFICATIONS);
             chromeOptions.addArguments(REMOTE_ALLOW_ORIGINS);
-
             if (configuration().headless()) chromeOptions.addArguments(CHROME_HEADLESS);
-
             return chromeOptions;
         }
     }, FIREFOX {
         @Override
         public WebDriver createLocalDriver() {
             WebDriverManager.firefoxdriver().setup();
-
             return new FirefoxDriver(getOptions());
         }
         @Override
@@ -93,64 +86,51 @@ public enum BrowserFactory {
                     .withCapabilities(new FirefoxOptions());
                 driverContainer.start();
                 return new RemoteWebDriver(driverContainer.getSeleniumAddress(), new FirefoxOptions());
-
-
         }
             @Override
         public FirefoxOptions getOptions() {
             var firefoxOptions = new FirefoxOptions();
             firefoxOptions.addArguments(START_MAXIMIZED);
-
             if (configuration().headless()) firefoxOptions.addArguments(GENERIC_HEADLESS);
-
             return firefoxOptions;
         }
     }, EDGE {
         @Override
         public WebDriver createLocalDriver() {
             WebDriverManager.edgedriver().setup();
-
             return new EdgeDriver(getOptions());
         }
-
         public WebDriver createTestContainerDriver() {
             BrowserWebDriverContainer<?> driverContainer = new BrowserWebDriverContainer<>()
                     .withCapabilities(new EdgeOptions());
                 driverContainer.start();
                 return new RemoteWebDriver(driverContainer.getSeleniumAddress(), new EdgeOptions());
-
         }
 
         @Override
         public EdgeOptions getOptions() {
             var edgeOptions = new EdgeOptions();
             edgeOptions.addArguments(START_MAXIMIZED);
-
             if (configuration().headless()) edgeOptions.addArguments(GENERIC_HEADLESS);
-
             return edgeOptions;
         }
     }, SAFARI {
         @Override
         public WebDriver createLocalDriver() {
             WebDriverManager.safaridriver().setup();
-
             return new SafariDriver(getOptions());
         }
         public WebDriver createTestContainerDriver() {
             throw new IllegalArgumentException(
                     "Browser Safari not supported on TestContainers yet");
-
         }
 
         @Override
         public SafariOptions getOptions() {
             var safariOptions = new SafariOptions();
             safariOptions.setAutomaticInspection(false);
-
             if (TRUE.equals(configuration().headless()))
                 throw new HeadlessNotSupportedException(safariOptions.getBrowserName());
-
             return safariOptions;
         }
     };
